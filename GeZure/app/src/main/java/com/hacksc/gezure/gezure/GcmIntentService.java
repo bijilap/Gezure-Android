b2b23d462b670a8a464f56939692c90f3548a800
unknown <bijilap@gmail.com>
2014-11-09 06:57:10 -0800

    Improved homepage

--- GeZure/app/src/main/java/com/hacksc/gezure/gezure/GcmIntentService.java
+++ GeZure/app/src/main/java/com/hacksc/gezure/gezure/GcmIntentService.java
@@ -6,7 +6,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -47,19 +46,15 @@ public class GcmIntentService extends IntentService {
             // If it's a regular GCM message, do some work.
             } else if (GoogleCloudMessaging.
                     MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                // This loop represents the service doing some work.
-                for (int i=0; i<5; i++) {
-                    Log.i(TAG, "Working... " + (i + 1)
-                            + "/5 @ " + SystemClock.elapsedRealtime());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
-                // Post notification of received message.
-                sendNotification("Received: " + extras.toString());
-                Log.i(TAG, "Received: " + extras.toString());
+                Log.d("DEBUG", extras.toString() + "YEAY");
+
+                Intent i = new Intent();
+                i.setClass(this, VideoPage.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                String url = (String) extras.get("test");
+                Log.d("DEBUG", url);
+                i.putExtra("url", url);
+                startActivity(i);
             }
         }
         // Release the wake lock provided by the WakefulBroadcastReceiver.
